c576c80e0aaf10fe800ce53383703701c14345cf
joshpson <jpearson846@gmail.com>
2018-07-02 16:29:27 -0400

    Done.

--- src/Tier1.js
+++ src/Tier1.js
@@ -1,27 +1,60 @@
-import React, { Component } from 'react'
-import { getRandomColor, getReducedColor } from './randomColorGenerator.js'
-import Tier2 from './Tier2'
-
+import React, { Component } from "react";
+import { getRandomColor, getReducedColor } from "./randomColorGenerator.js";
+import Tier2 from "./Tier2";
 
 export default class Tier1 extends Component {
-
   constructor(props) {
-    super(props)
-    const initialColor = getRandomColor()
+    super(props);
     this.state = {
-      color: initialColor,
-      childColor: getReducedColor(initialColor)
-    }
+      color: this.props.color,
+      childColor: getReducedColor(this.props.color),
+      grandChildColor: getReducedColor(getReducedColor(this.props.color))
+    };
   }
+
+  handleClick = e => {
+    e.stopPropagation();
+    let newColor = getRandomColor();
+    this.setState({
+      color: newColor,
+      childColor: getReducedColor(newColor)
+    });
+  };
+
+  changeChildColor = color => {
+    this.setState({
+      childColor: color
+    });
+  };
+
+  changeGrandchildColor = color => {
+    this.setState({
+      grandChildColor: color
+    });
+  };
 
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div onClick={() => {this.setState({color: "#000"})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 color={"#0F0"} />
-        <Tier2 color={"#0FF"} />
+      <div
+        onClick={this.handleClick}
+        className="tier1"
+        style={{ backgroundColor: this.state.color, color: this.state.color }}
+      >
+        <Tier2
+          color={this.state.childColor}
+          setColor={this.changeChildColor}
+          childColor={this.state.grandChildColor}
+          setChildColor={this.changeGrandchildColor}
+        />
+        <Tier2
+          color={this.state.childColor}
+          setColor={this.changeChildColor}
+          childColor={this.state.grandChildColor}
+          setChildColor={this.changeGrandchildColor}
+        />
       </div>
-    )
+    );
   }
 }
